8b69da0299775653a77afc59d229ff827aa79432
Sebazt <juan.londono29@udea.edu.co>
2022-11-07 09:18:34 -0500

    add funcionts in Heroe component

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -3,13 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.scss']
+  styleUrls: ['./heroe.component.scss'],
 })
 export class HeroeComponent implements OnInit {
+  public nombre: string = 'Capitan America';
+  public edad: number = 29;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public getName(): string {
+    return this.nombre + '-' + this.edad.toString();
   }
 
+  public getNameCapitalizado(): string {
+    return this.nombre.toUpperCase();
+  }
+
+  get nameCapitalizado(): string {
+    return this.getName().toUpperCase();
+  }
+
+  public changeName():void{
+    this.nombre = 'Spiderman';
+  }
+
+  public changeEdad():void{
+    this.edad = 30;
+  }
+
+  public returnPropertys():void{
+    this.nombre = this.nombre;
+    this.edad = this.edad;
+    console.log(this.nombre);
+
+  }
 }
